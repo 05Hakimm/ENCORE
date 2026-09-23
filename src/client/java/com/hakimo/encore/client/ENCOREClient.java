dade1643ed2a8c4fc62189aa1dfc941aa5f08317
05Hakimm <2024704000@lacatholille.fr>
2026-09-23 21:29:13 +0200

    Initial Fabric mod template

---
+++ src/client/java/com/hakimo/encore/client/ENCOREClient.java
@@ -0,0 +1,10 @@
+package com.hakimo.encore.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ENCOREClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
